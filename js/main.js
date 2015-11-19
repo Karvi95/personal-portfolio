cd801df71efc1a64bd56a74d02d81954ab0f26f8
Arvindram Krishnamoorthy <karvi90@yahoo.com>
2015-11-19 13:52:35 -0800

    Multipage works

---
+++ js/main.js
@@ -0,0 +1,47 @@
+// Create app
+var myApp = angular.module('myApp', ['ui.router'])
+// Configure app
+myApp.config(function($stateProvider){
+    $stateProvider
+        .state('home', {
+            url:'/',
+            templateUrl: 'templates/home.html',
+            controller: 'HomeController',
+        }).state('projects',{
+            url:'/projects',
+            templateUrl:'templates/projects.html',
+            controller: 'projectsController',
+        }).state('about',{
+            url:'/about',
+            templateUrl:'templates/about.html',
+            controller:'AboutController'
+        })
+})
+
+// Landing page controller: define $scope.number as a number
+.controller('HomeController',function($scope, $http){
+})
+
+// About page controller: define $scope.about as a string
+.controller('AboutController', function($scope, $http){
+    $http.get("data/intro.json").success(function(response) {
+        $scope.intro = data;
+    })   
+    $http.get("data/hobbies.json").success(function(response) {
+        $scope.hobbies = dataOne;
+    })
+    $http.get("data/interests.json").success(function(response) {
+        $scope.interests = dataTwo;
+    })
+})
+
+
+// projects controller: define $scope.url as an image
+.controller('projectsController', function($scope, $http){
+    $http.get("data/codingExperience.json").success(function(response) {
+        $scope.exp = response;
+    })
+    $http.get("data/projectsList.json").success(function(response) {
+        $scope.projects = response;
+    })
+})
